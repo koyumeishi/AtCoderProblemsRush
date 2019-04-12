59f0ce1146d21cb7cfa15ee0e1d51f40f5132ba4
koyumeishi <koyumeishi@users.noreply.github.com>
2019-04-13 02:10:43 +0900

    add new atcoderProblems apps file
    not implemented yet

---
+++ src/kenkooo/ApplicationAtCoderProblems.tsx
@@ -0,0 +1,85 @@
+import { IApplication, strageKeyAC, strageKeyWA } from '../struct/IApplication';
+import { Result } from '../struct/Result';
+import { Submission } from '../struct/Submission';
+import { SubmissionsCollection } from '../struct/SubmissionsCollection';
+import { ScraperAtCoderProblems } from './ScraperAtCoderProblems';
+
+export class ApplicationAtCoderProblems implements IApplication {
+  public submissionSetAC: SubmissionsCollection;
+  public submissionSetWA: SubmissionsCollection;
+  constructor () {
+    this.submissionSetAC = new SubmissionsCollection();
+    this.submissionSetWA = new SubmissionsCollection();
+  }
+
+  public save (saveAC: boolean, saveWA: boolean): void {
+    if (saveAC) GM_setValue(strageKeyAC, this.submissionSetAC.marshal());
+    if (saveWA) GM_setValue(strageKeyWA, this.submissionSetWA.marshal());
+  }
+  public load (): void {
+    this.submissionSetAC.unmarshal(GM_getValue(strageKeyAC, '[]'));
+    this.submissionSetWA.unmarshal(GM_getValue(strageKeyWA, '[]'));
+  }
+
+  public updateSubmissions (): boolean {
+    const scraper: ScraperAtCoderProblems = new ScraperAtCoderProblems();
+    if (scraper.doneFetchingProblemSet(document) === false) return false;
+
+    let submissions = scraper.scrape(document);
+
+    this.load();
+    const userIds: Set<string> = this.savedUserIds();
+    submissions = submissions.filter(s => userIds.has(s.userId));
+    const newAC: boolean = this.submissionSetAC
+      .insertAll(submissions.filter((s: Submission) => s.result === Result.AC));
+    const newWA: boolean = this.submissionSetWA
+      .insertAll(submissions.filter((s: Submission) => s.result === Result.WRONG));
+    this.save(newAC, newWA);
+
+    return true;
+  }
+
+  public applySavedSubmissions (): void {
+    const scraper: ScraperAtCoderProblems = new ScraperAtCoderProblems();
+    const submissions = scraper.scrape(document);
+    this.load();
+
+    const problemMapping: Map<string, Element> = scraper.makeProblemIdToTableCellMap(document);
+    const ac: Submission[] = this.submissionSetAC.submissions.filter(s => s.userId === scraper.userId);
+    const wa: Submission[] = this.submissionSetWA.submissions.filter(s => s.userId === scraper.userId);
+
+    function setWA(s: Submission): void {
+      try {
+        problemMapping.get(scraper.makeProblemKey(s.contestId, s.problemId))
+          .classList.add('warning');
+      } catch (e) {
+        return;
+      }
+    }
+
+    function setAC(s: Submission): void {
+      try {
+        const list: DOMTokenList = problemMapping.get(scraper.makeProblemKey(s.contestId, s.problemId))
+          .classList;
+        list.remove('warning');
+        list.add('success');
+      } catch (e) {
+        return;
+      }
+    }
+
+    this.submissionSetWA.submissions
+      .filter(s => s.userId === scraper.userId)
+      .forEach(setWA);
+    this.submissionSetAC.submissions
+      .filter(s => s.userId === scraper.userId)
+      .forEach(setAC);
+  }
+
+  private savedUserIds (): Set<string> {
+    return new Set<string>(
+      this.submissionSetAC.submissions.map(s => s.userId)
+      .concat(this.submissionSetWA.submissions.map(s => s.userId)));
+  }
+
+}
---
+++ src/kenkooo/ScraperAtCoderProblems.tsx
@@ -0,0 +1,86 @@
+import { Result } from '../struct/Result';
+import { createSubmission, Submission } from '../struct/Submission';
+
+export class ScraperAtCoderProblems {
+  public userId: string;
+  constructor () {
+    this.userId = this.parseUserId(document.location.href);
+  }
+
+  public parseUserId(url: string): string {
+    const reg: RegExp = /^https:\/\/kenkoooo\.com\/atcoder\/#\/table\/([\w-]+)/;
+    if (reg.test(url) === false) {
+      return '';
+    }
+    const users = reg.exec(url).split('/');
+    return users[0];
+  }
+
+  public isProblemUrl(url: string): boolean {
+    return /atcoder\.jp\/contests\/.+?\/tasks\/.+$/.test(url);
+  }
+  public parseContestId(url: string): string {
+    return /atcoder\.jp\/contests\/(.+?)\/tasks\/.+$/.exec(url)[1];
+  }
+  public parseProblemId(url: string): string {
+    return /atcoder\.jp\/contests\/.+?\/tasks\/(.+)$/.exec(url)[1];
+  }
+  public parseVerdict(td: Element): Result {
+    const classArray: string[] = Array.from(td.classList);
+    if (classArray.includes('table-success')) return Result.AC;
+    if (classArray.includes('table-warning')) return Result.WRONG;
+    return Result.NODATA;
+  }
+
+  public parseTableCell (td: Element): Submission[] {
+    try {
+      const url: string = td.querySelector('a').href;
+      if (this.isProblemUrl(url) === false) return [] as Submission[];
+      const verdict: Result = this.parseVerdict(td);
+      return [createSubmission(
+        this.parseContestId(url),
+        this.parseProblemId(url),
+        verdict,
+        this.userId)];
+    } catch (e) {
+      return [] as Submission[];
+    }
+  }
+
+  public parseTableRow (tr: Element): Submission[] {
+    return [].concat(...Array.from(tr.querySelectorAll('td'))
+      .map(td => this.parseTableCell(td)))
+      .filter((s: Submission) => s.result !== Result.NODATA);
+  }
+
+  public scrape (doc: Document): Submission[] {
+    if (this.userId === '') return [];
+    return [].concat(...Array.from(doc.querySelectorAll('tbody > tr'))
+      .map(tr => this.parseTableRow(tr)));
+  }
+
+  public doneFetchingProblemSet (doc: Document): boolean {
+    return Array.from(doc.querySelector('tbody > tr').querySelectorAll('a'))
+      .map(e => e.href).some(url => this.isProblemUrl(url));
+  }
+
+  public makeProblemKey (contestId: string, problemId: string): string {
+    return `${problemId}/${contestId}`;
+  }
+
+  public makeProblemIdToTableCellMap (doc: Document): Map<string, Element> {
+    const res: Map<string, Element> = new Map<string, Element>();
+
+    const td: Element[] = [].concat(...Array.from(doc.getElementsByTagName('tbody'))
+      .map(e => Array.from(e.getElementsByTagName('td'))));
+
+    const problemOfCell: Submission[][] = td.map(e => this.parseTableCell(e));
+
+    problemOfCell.forEach((p: Submission[], i: number) => {
+      if (p.length === 0) return;
+      const key: string = this.makeProblemKey(p[0].contestId, p[0].problemId);
+      res.set(key, td[i]);
+    });
+    return res;
+  }
+}
